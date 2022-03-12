39c55862d09cf94fca21de2dc3c90e4865ff27d2
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-12 18:50:20 +0900

    feat: 동작확인을 위한 간단한 예제(Member를 저장하고 불러오는 테스트 코드) 구현

---
+++ src/main/java/jpabook2/jpashop2/MemberRepository.java
@@ -0,0 +1,24 @@
+package jpabook2.jpashop2;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext // springboot에서 엔티티 매니저 객체를 만들어 준다
+    private EntityManager em;
+
+    // Member 저장
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/jpabook2/jpashop2/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook2.jpashop2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+
+        // given
+
+        Member member = new Member();
+        member.setUsername("유저네임테스트");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
