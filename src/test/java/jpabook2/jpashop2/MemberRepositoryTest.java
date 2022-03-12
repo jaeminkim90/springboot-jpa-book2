bdadbd8bd18e397c78f496adca9ea1632d5777d5
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-12 19:05:03 +0900

    feat: trace와 p6spy를 이용하여 쿼리 내용을 모니터링

--- src/test/java/jpabook2/jpashop2/MemberRepositoryTest.java
+++ src/test/java/jpabook2/jpashop2/MemberRepositoryTest.java
@@ -1,15 +1,15 @@
 package jpabook2.jpashop2;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @RunWith(SpringRunner.class)
@@ -21,10 +21,10 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false)
     public void testMember() throws Exception {
 
         // given
-
         Member member = new Member();
         member.setUsername("유저네임테스트");
 
@@ -35,5 +35,7 @@ class MemberRepositoryTest {
         // then
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member = " + (findMember == member));
     }
 }
