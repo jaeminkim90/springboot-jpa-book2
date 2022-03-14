1ed6049d8434c87a1e8f993dd00f4ce5e7291d98
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-14 15:26:33 +0900

    feat: 값 타입은 변경 불가능하게 설계하고, JPA 스펙을 고려하여 기본 생성자 추가

--- src/main/java/jpabook2/jpashop2/domain/Address.java
+++ src/main/java/jpabook2/jpashop2/domain/Address.java
@@ -12,4 +12,15 @@ public class Address {
     private String street;
     private String zipcode;
 
+    // 기본 생성자도 필요하다. 리플렉션이나 프록시에 사용된다. protected를 쓰는게 그나마 더 안전하다.
+    protected Address(String city) {
+        this.city = city;
+    }
+
+    // @Setter 제공하지 않고, 생성자에서 값을 모두 초기화해서 변경 불가능한 클래스를 만드는 것이 좋은 설계 방법이다.
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
