843097055bea8c656cf6e355b320fca40d9861e0
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-14 14:47:16 +0900

    feat: item과 categoty는 다대다 관계로 매핑

--- src/main/java/jpabook2/jpashop2/domain/item/Item.java
+++ src/main/java/jpabook2/jpashop2/domain/item/Item.java
@@ -1,9 +1,12 @@
 package jpabook2.jpashop2.domain.item;
 
+import jpabook2.jpashop2.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 부모 클래스 전략 설정
@@ -19,5 +22,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    private List<Category> categories = new ArrayList<>();
 
 }
