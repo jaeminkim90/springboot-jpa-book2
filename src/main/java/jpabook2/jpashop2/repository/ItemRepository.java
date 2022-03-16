8b6239c61036ed2809c0da954fb040cb6ab66354
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-16 13:01:14 +0900

    feat: ItemRepository애 아이템을 저장하는 api 구현

---
+++ src/main/java/jpabook2/jpashop2/repository/ItemRepository.java
@@ -0,0 +1,22 @@
+package jpabook2.jpashop2.repository;
+
+import jpabook2.jpashop2.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item); // update와 비슷하다
+        }
+    }
+}
