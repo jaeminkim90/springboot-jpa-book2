6925e6441e5ee5acd01e080d7fcbe45f1eda90e4
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-17 14:04:18 +0900

    feat: Order 엔티티 안에 주문의 생성, 취소, 전체 주문 가격을 조회하는 메서드 구현

--- src/main/java/jpabook2/jpashop2/domain/Order.java
+++ src/main/java/jpabook2/jpashop2/domain/Order.java
@@ -66,4 +66,29 @@ public class Order {
         return order;
     }
 
+    // == 비즈니스 로직 == //
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMPLETE) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.getStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // == 조회 로직 == //
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+
+    }
 }
