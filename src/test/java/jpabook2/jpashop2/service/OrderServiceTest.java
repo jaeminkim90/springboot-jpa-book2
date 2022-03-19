86acd8c8c3d71d143f2512307c7a800e6167390e
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-19 13:45:05 +0900

    test: 상품의 재고 수량이 초과되었을 때 예외가 정상적으로 발생하는지 확인하는 테스트

--- src/test/java/jpabook2/jpashop2/service/OrderServiceTest.java
+++ src/test/java/jpabook2/jpashop2/service/OrderServiceTest.java
@@ -6,8 +6,8 @@ import jpabook2.jpashop2.domain.Order;
 import jpabook2.jpashop2.domain.OrderStatus;
 import jpabook2.jpashop2.domain.item.Book;
 import jpabook2.jpashop2.domain.item.Item;
+import jpabook2.jpashop2.exception.NotEnoughStockException;
 import jpabook2.jpashop2.repository.OrderRepository;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,23 +24,19 @@ import static org.junit.Assert.*;
 @Transactional
 public class OrderServiceTest {
 
-    @Autowired EntityManager em;
-    @Autowired OrderService orderService;
-    @Autowired OrderRepository orderRepository;
+    @Autowired
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
 
     @Test
     public void 상품주문() throws Exception {
         // given
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "강남대로", "123-123"));
-        em.persist(member);
+        Member member = createMember();
 
-        Book book = new Book();
-        book.setName("시골개발자의 JPA 여행");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Book book = createBook("시골개발자의 JPA 여행", 10000, 10);
 
         int orderCount = 2;
 
@@ -56,6 +52,24 @@ public class OrderServiceTest {
         assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, book.getStockQuantity());
     }
 
+
+
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과() throws Exception {
+        // given
+        Member member = createMember();
+        Item item = createBook("시골개발자의 JPA 여행", 10000, 10);
+
+        int orderCount = 11; // 예외가 터져야 한다
+
+        // when
+        orderService.order(member.getId(), item.getId(), orderCount);
+        // 수량이 0보다 작을 경우 NotEnoughStockException이 발생한다
+
+        // then
+        fail("재고 수량 부족 예외가 발생해야 한다."); // 이 라인까지 테스트가 진행되면 안된다
+    }
+
     @Test
     public void 주문취소() throws Exception {
 
@@ -66,14 +80,22 @@ public class OrderServiceTest {
         // then
     }
 
-    @Test
-    public void 상품주문_재고수량초과() throws Exception {
-
-        // given
-
-        // when
-
-        // then
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
     }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강남대로", "123-123"));
+        em.persist(member);
+        return member;
+    }
+
 
 }
