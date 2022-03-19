df22ea97b3ca934dd8cb582d74943aac5ffc214f
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-19 18:21:56 +0900

    feat: JPQL 쿼리를 문자로 생성하여 동적 쿼리 구현

--- src/main/java/jpabook2/jpashop2/repository/OrderRepository.java
+++ src/main/java/jpabook2/jpashop2/repository/OrderRepository.java
@@ -3,8 +3,10 @@ package jpabook2.jpashop2.repository;
 import jpabook2.jpashop2.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -23,14 +25,53 @@ public class OrderRepository {
 
     // 검색용으로 나중에 구현 예정
     public List<Order> findAll(OrderSearch orderSearch) {
-        return em.createQuery("select o from Order o join o.member m" +
-                        " where o.status = :status" +
-                        " and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setFirstResult(100) // 페이징을 사용할 때 시작 지점이 된다
-                .setMaxResults(1000) // 조회결과를 최대 1000개로 제한한다
-                .getResultList();
 
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) { // status가 존재하면
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000); //최대 1000건
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
     }
+
+//        return em.createQuery("select o from Order o join o.member m" +
+//                        " where o.status = :status" +
+//                        " and m.name like :name", Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setFirstResult(100) // 페이징을 사용할 때 시작 지점이 된다
+//                .setMaxResults(1000) // 조회결과를 최대 1000개로 제한한다
+//                .getResultList();
+}
 }
