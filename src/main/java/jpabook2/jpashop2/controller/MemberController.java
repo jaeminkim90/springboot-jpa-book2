d444c07130e14ee237e959e081ce1a48c38a97ed
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-20 12:50:09 +0900

    feat: form의 내용을 전달받아 Member를 새로 만들고 저장하는 컨트롤러 구현

--- src/main/java/jpabook2/jpashop2/controller/MemberController.java
+++ src/main/java/jpabook2/jpashop2/controller/MemberController.java
@@ -1,10 +1,15 @@
 package jpabook2.jpashop2.controller;
 
+import jpabook2.jpashop2.domain.Address;
+import jpabook2.jpashop2.domain.Member;
 import jpabook2.jpashop2.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,6 +21,21 @@ public class MemberController {
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form) {
+
+        // form에서 입력 받은 데이터로 Address 객체를 완성한다
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member); // 저장
+        return "redirect:/"; // 홈으로 리다이렉트 보낸다
 
     }
+
 }
