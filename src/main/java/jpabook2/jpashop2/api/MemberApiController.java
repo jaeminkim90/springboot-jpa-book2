b7e158a81e07fbdfa11401370e283a5056445169
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-21 20:11:40 +0900

    feat: 엔티티 전체를 JSON으로 반환하는 회원 조회 V1 구현

--- src/main/java/jpabook2/jpashop2/api/MemberApiController.java
+++ src/main/java/jpabook2/jpashop2/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController // 데이터는 JSON이나 XML로 바로 보내고자 할 때 쓰는 Annotation
 @RequiredArgsConstructor
@@ -16,15 +17,14 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    // 회원 조회 v1 : 전체 member를 반환하는 API
+    @GetMapping("/api/v1/members")
+    public List<Member> memberV1() {
+        return memberService.findMembers();
+    }
+
     /**
      * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
-     * 문제점
-     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
-     * - 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
-     * - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한 모든 요청 요구사항을 담기는 어렵다.
-     * - 엔티티가 변경되면 API 스펙이 변한다.
-     * 결론
-     * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
      */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -71,8 +71,6 @@ public class MemberApiController {
         private Long id;
         private String name;
     }
-
-
 
 
     @Data // 회원 생성 시 요청에 사용하는 DTO
