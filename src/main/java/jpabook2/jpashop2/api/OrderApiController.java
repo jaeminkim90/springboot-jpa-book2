d8003fbac36e61681a9fdf534d585041a1be90b6
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-28 13:32:17 +0900

    feat: Dto로 값을 반환하긴 하지만, Dto안에 엔티티가 포함되어 있는 잘못된 방식의 API

--- src/main/java/jpabook2/jpashop2/api/OrderApiController.java
+++ src/main/java/jpabook2/jpashop2/api/OrderApiController.java
@@ -1,13 +1,25 @@
 package jpabook2.jpashop2.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+import jpabook2.jpashop2.domain.Address;
 import jpabook2.jpashop2.domain.Order;
 import jpabook2.jpashop2.domain.OrderItem;
+import jpabook2.jpashop2.domain.OrderStatus;
 import jpabook2.jpashop2.repository.OrderRepository;
 import jpabook2.jpashop2.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * V1. 엔티티 직접 노출 - 엔티티가 변하면 API 스펙이 변한다. - 트랜잭션 안에서 지연 로딩 필요 - 양방향 연관관계 문제 * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X) - 트랜잭션 안에서 지연 로딩 필요 V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O) - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
+ * * V4.JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1+NQuery) - 페이징 가능 V5.JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1+1Query) - 페이징 가능 V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query) - 페이징 불가능... *
+ */
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +27,10 @@ public class OrderApiController {
 
 	private final OrderRepository orderRepository;
 
+
+	/**
+	 * V1. 엔티티 직접 노출 - Hibernate5Module 모듈 등록, LAZY=null 처리 * - 양방향 관계 문제 발생 -> @JsonIgnore
+	 */
 	@GetMapping("/api/v1/orders")
 	public List<Order> ordersV1() {
 		List<Order> all = orderRepository.findAllbyString(new OrderSearch());
@@ -26,4 +42,36 @@ public class OrderApiController {
 		}
 		return all;
 	}
+
+	@GetMapping("/api/v2/orders")
+	public List<OrderDto> orderV2() {
+		List<Order> orders = orderRepository.findAllbyString(new OrderSearch());
+		List<OrderDto> collect = orders.stream()
+			.map(o -> new OrderDto(o))
+			.collect(Collectors.toList());
+		return collect;
+	}
+
+	@Data
+	static class OrderDto {
+
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+		private List<OrderItem> orderItems;
+
+		public OrderDto(Order order) {
+			orderId = order.getId();
+			name = order.getMember().getName();
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress();
+			order.getOrderItems().stream().forEach(o -> o.getItem().getName()); // 프록시 초기화
+
+			orderItems = order.getOrderItems();
+		}
+	}
+
 }
